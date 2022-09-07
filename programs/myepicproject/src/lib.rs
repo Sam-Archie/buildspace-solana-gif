a73673bba025abf7595c03f4045deeae9f9f494b
Samuel Armitt-Fior <68110533+Sam-Archie@users.noreply.github.com>
2022-09-07 11:03:23 +0100

    base project complete

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6gweAUcgHUfiUkD2SPGpE7MBjh8hQ7mPZG5iQrBqP46n");
 
 #[program]
 pub mod myepicproject {
@@ -12,10 +12,19 @@ pub mod myepicproject {
   }
   
 	// Another function woo!
-  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
-    // Get a reference to the account and increment total_gifs.
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result <()> {    // Get a reference to the account and increment total_gifs.
     let base_account = &mut ctx.accounts.base_account;
-    base_account.total_gifs += 1;
+    let user = &mut ctx.accounts.user;
+
+    let item = ItemStruct {
+        gif_link: gif_link.to_string(),
+        user_address: *user.to_account_info().key,
+      };
+          
+      // Add it to the gif_list vector.
+      base_account.gif_list.push(item);
+      base_account.total_gifs += 1;
+
     Ok(())
   }
 }
@@ -35,9 +44,18 @@ pub struct StartStuffOff<'info> {
 pub struct AddGif<'info> {
   #[account(mut)]
   pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
 }
 
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
 }
